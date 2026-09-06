fb90155bdf6415c6fd027006539da0ccac39fba6
Maksim124512M <max1996az@gmail.com>
2026-09-06 18:31:01 +0300

    feat: add first functionals for training and loading model > > Created functions for training algorithm and dumping model in .pkl format

---
+++ src/train.py
@@ -0,0 +1,20 @@
+import joblib
+
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import IsolationForest
+from sklearn.model_selection import GridSearchCV
+
+def train_model(X_train, X_test, artifact_path='models/isolation_forest.pkl') -> dict:
+    model = IsolationForest(
+        n_estimators=300,
+        contamination=0.02,
+        random_state=42,
+    )
+    model.fit(X_train)
+    test_predictions = model.predict(X_test)
+
+    joblib.dump(model, artifact_path)
+
+    return {
+        'model': model,
+    }
